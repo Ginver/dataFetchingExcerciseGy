7fc7d428c06cb3a52eaf51b3ea6c83f9391b119d
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-14 14:38:46 +0100

    list of name, height, skin_color is logged from API

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ async function displayStarWars() {
     // console.log(Hi!)
     const response = await axios.get("https://swapi.dev/api/people/"
     );
+    console.log("RES:", response.data.results);
+    const warsOfStars = document.getElementById("starWars");
+    console.log(warsOfStars);
 
+    for (const result of response.data.results) {
+        console.log(result);
+        const starWarsList = document.createElement("li");
+
+        starWarsList.textContent = `${result.name} - ${result.height} - ${result.skin_color}`
+        console.log(starWarsList);
+
+        warsOfStars.appendChild(starWarsList);
+    }
 }
 
 displayStarWars();
