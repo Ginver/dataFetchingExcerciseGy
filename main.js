32e96b65386c55bd1e8c0c5aee68f1da743e8b06
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-14 14:26:29 +0100

    async function created url is added

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-//console.log(axios);
+console.log(axios);
 
-async function displayPrizes() {
+async function displayStarWars() {
     // console.log(Hi!)
-    const response = await axios.get("https://ignoble-api.herokuapp.com/prizes?year=2020"
+    const response = await axios.get("https://swapi.dev/api/people/"
     );
 
-    console.log(response.data.prizes);
-    const prizeList = document.getElementById("prize-list");
-
-    for (const prize of response.data.prizes) {
-        // console.log("PRIZE:", prize);
-        const listItem = document.createElement("li");
-
-        listItem.textContent = `${prize.year} - ${prize.type} - ${prize.description}`;
-        // console.log("LISTITEM", listItem);
-
-        prizeList.appendChild(listItem);
-    }
 }
 
-displayPrizes();
+displayStarWars();
 
